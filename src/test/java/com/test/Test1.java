e2a43270deadb8da91615f0e4bd28480979e3f38
ArunPrasanthA <arunprasanth.arr1@gmail.com>
2023-09-05 18:57:31 +0530

    initial codes

---
+++ src/test/java/com/test/Test1.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class Test1 {
+	public void method1() {
+		System.out.println("1");
+
+	}
+	public void method2() {
+		System.out.println("2");
+
+	}
+	public void method3() {
+		System.out.println("3");
+
+	}
+	public void method4() {
+		System.out.println("4");
+
+	}
+
+}
